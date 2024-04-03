36b10fff5bba4ff771e9eb8d79305f0fc4ae6268
Tim Green <itimky@gmail.com>
2024-04-03 08:31:36 +0400

    run server

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	ordersdb "github.com/itimky/gopher-up-march-2024/pkg/orders/db"
 	ordersdomain "github.com/itimky/gopher-up-march-2024/pkg/orders/domain"
 	ordershttp "github.com/itimky/gopher-up-march-2024/pkg/orders/http"
@@ -10,5 +13,8 @@ func main() {
 	db := ordersdb.NewInMemDB()
 	orders := ordersdomain.NewOrders(db)
 	httpHandler := ordershttp.NewHandler(orders)
-	_ = httpHandler
+
+	if err := http.ListenAndServe(":8080", httpHandler.Mux()); err != nil {
+		log.Fatal(err)
+	}
 }
